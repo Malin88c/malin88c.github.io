1ff579675b513b3970db4e08bfb3bfe493807b80
Malin Classon <m.classon@hotmail.com>
2017-02-17 11:48:09 +0100

    changes

--- ovning14.js
+++ ovning14.js
@@ -10,15 +10,14 @@
 
 	class TranslateList extends React.Component {
 
-		const newList = this.props.lista.map(item => < li key = {
-				item.id
-			} > {
-				item.katt
-			} = {
-				item.translation
-			} < /li> )
-
-			render() {
+		render() {
+			const newList = this.props.lista.map(item => ( < li key = {
+					item.id
+				} > () {
+					item.katt
+				} = {
+					item.translation
+				}) < /li> )
 				return ( < ul > {
 						newList
 					} < /ul> )
