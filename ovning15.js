0b1e4cc7cb193af1d29ef72bcb3575e5b7296ddf
Malin Classon <m.classon@hotmail.com>
2017-02-20 21:58:48 +0100

    ..

--- ovning15.js
+++ ovning15.js
@@ -1,58 +1,43 @@
 class App extends React.Component {
+
   constructor(props) {
     super(props);
-    this.inputChange = this.inputChange.bind(this);
-    this.state = {
+    this inputChange = this.inputChange.bind(this);
+    this state = {
       inputValue: ''
     };
   }
   inputChange(event) {
-    console.log('App.inputChange körs')
     this.setState({
       inputValue: event.target.value
     })
   }
-  render () {
-    return (
-      <div id="app">
-      <FormComponent>
-      <button>button</button> <br/>
-      <InputComponent changeEvent={this.inputChange} />
-      </FormComponent>
-      <Info text={this.state.inputValue} />
-      </div>
-    );
-  }
-}
-
-class FormComponent extends React.Component {
   render() {
     return (
-      <form>
-      {this.props.children}
-      </form>
-    );
+      <div id='app'>
+			<FormComponent/>
+			</div>
+    )
   }
 }
+class FormComponent extends React.Component {
 
+  render() {
+    return (
+      <form>this.props.children</form>)
+  };
+}
 
 class InputComponent extends React.Component {
   render() {
     return (
-      <input 
-        onChange={this.props.changeEvent} />
-    );
-  }
-}
-class Info extends React.Component {
-  render() {
-    return (
-      <div>{this.props.text}</div>
-    );
+      <input type='text' onChange={this.props.changeEvent}/>
+    )
   }
 }
 
-ReactDOM.render(
+ReactDOM.render {
   <App/>,
-  document.getElementById('app')
-);
+  document.getElementById('app');
+
+}
