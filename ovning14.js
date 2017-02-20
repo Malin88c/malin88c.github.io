fc6d8c78638f9af788db5956106535e6effe1c97
Malin Classon <m.classon@hotmail.com>
2017-02-20 16:36:42 +0100

    .

--- ovning14.js
+++ ovning14.js
@@ -1,20 +1,3 @@
-	const list = [{
-		word: 'katt'
-		, translation: 'cat'
-		, id: 1
-	}, {
-		word: 'hav'
-		, translation: 'ocean'
-		, id: 2
-	}];
-
-
-class TranslatedList extends React.Component{
-	render(){
-		return (<p>{list}<p>);
-	}
-}
-
 
       ReactDOM.render(
         <h1>TEST</h1>,
