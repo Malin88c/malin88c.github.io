d8c1562ace8c5404ae08ee9758ed86288ce1f140
Malin Classon <m.classon@hotmail.com>
2017-02-24 09:32:26 +0100

    ..

--- ovning12.js
+++ ovning12.js
@@ -4,7 +4,9 @@
 			buttonFetch.addEventListener('click', function () {
 
             console.log('knappen trycktes på');
-				let url = 'http://forverkliga.se/JavaScript/api/simple.php' +'?world=whatever'; ;
+				let url = 'http://forverkliga.se/JavaScript/api/simple.php' +'?world=whatever'; 
+				let one = document.getElementById('poulation');
+				let totalPopulation;
 
 
 				fetch(url)
@@ -15,13 +17,19 @@
 						let div = document.getElementById('box');
 
                         for (var i = 0; i < json.length; i++){
-                        var obj = json[i];
-                        for (var x in obj){
-                        div.innerHTML += `${x}: ${obj[x]}<br/>`;
-                            }};
+                        	var obj = json[i];
+                        		for (var x in obj){
+									if(x === 'population'){
+										totalPopulation  += x.value;
+									}
 
-
-					});
+                        			/*div.innerHTML += `${x}: ${obj[x]}<br/>`;*/
+                            	}};
+								return totalPopulation;
+					})
+					.then(function(pop){
+						one.innerHTML = 'I världen finns det ' + pop + ' människor'; 
+					})
 
 			});
         });
