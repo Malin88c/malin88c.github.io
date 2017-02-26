e2ec124d4841dfab972168c94efc5e889ae0f010
Malin Classon <m.classon@hotmail.com>
2017-02-26 22:06:23 +0100

    Övning 14

--- ovning14.js
+++ ovning14.js
@@ -1,24 +1,45 @@
-	const list = [{
-		word: 'katt'
-		, translation: 'cat'
-		, id: 1
-	}, {
-		word: 'hav'
-		, translation: 'ocean'
-		, id: 2
-	}];
+
+				const list = [{
+				word: 'katt'
+				, translation: 'cat'
+				, id: 1
+			}, {
+				word: 'hav'
+				, translation: 'ocean'
+				, id: 2
+			},
+			{
+				word: 'katt'
+				, translation: 'cat'
+				, id: 3
+			},
+			{
+				word: 'häst'
+				, translation: 'horse'
+				, id: 4
+			},
+			{
+				word: 'apa'
+				, translation: 'monkey'
+				, id: 5
+			},
+			{
+				word: 'ananas'
+				, translation: 'pineapple'
+				, id: 6
+			}];
 
 
-class TranslatedList extends React.Component{
+		class TranslatedList extends React.Component{
 
-render(){
-const newList = this.props.lista.map(item => <li key ={item.id}>{item.word + ' = ' + item.translation}</li>);
-return <ol>{newList}</ol>
-}
+		render(){
+		const newList = this.props.lista.map(item => <li key ={item.id}>{item.word + ' = ' + item.translation}</li>);
+		return <ol>{newList}</ol>
+		}
 
-}
+		}
 
-ReactDOM.render
-(
-  <TranslatedList lista={list}/>, document.getElementById('react-app')
-);
+		ReactDOM.render
+		(
+		<TranslatedList lista={list}/>, document.getElementById('react-app')
+		);
