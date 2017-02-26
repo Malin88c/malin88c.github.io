e5b55f95eafd3515244bd00e4f08ee9d12e72751
Malin Classon <m.classon@hotmail.com>
2017-02-26 22:16:58 +0100

    övning 10

--- ovning10.js
+++ ovning10.js
@@ -2,10 +2,8 @@ var btn = document.getElementById('knappbtn');
 var input = document.getElementById('message');
 
 
-document.addEventListener('click', addToList(input, btn));
-
-var addToList = function(input, btn){
+document.addEventListener('click', function(input, btn){
 	var lista = document.getElementById('lista');
 	
 	lista.push(input.innerHTML);
-}
+});
