6908edac153f982b701fe3e615ef56e280120868
Malin Classon <m.classon@hotmail.com>
2017-02-26 23:12:59 +0100

    ..

--- ovning10.js
+++ ovning10.js
@@ -1,9 +1,42 @@
-var btn = document.getElementById('knappbtn');
-var input = document.getElementById('message');
+window.addEventListener('load', function () {
+
+	var btn = document.getElementById('knappbtn');
+	var list = document.getElementById('lista');
 
 
-document.addEventListener('click', function(input, btn){
-	var lista = document.getElementById('lista');
-	
-	lista.push(input.innerHTML);
+	btn.addEventListener('click', function () {
+
+		var input = document.getElementById('message').value;
+
+		var lista = document.getElementById('lista');
+
+		var item = document.createElement('li');
+
+		item.appendChild(document.createTextNode(input));
+		lista.appendChild(item);
+	});
+
+	lista.addEventListener('click', function (e) {
+		var textBox = document.getElementById('message');
+		textBox.value = e.target.innerText;
+		var listItems = list.getElementsByTagName('li');
+
+		for (let i of listItems) {
+			if(i !== e.target){
+				i.style.background = 'none';
+			}
+			
+		}
+
+		if (e.target.style.backgroundColor !== 'hotpink') {
+			e.target.style.backgroundColor = 'hotpink';
+		}
+
+		else{
+			e.target.style.background = 'none';
+			textBox.value = '';
+		}
+
+	});
+
 });
