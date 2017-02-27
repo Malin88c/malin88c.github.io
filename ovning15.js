ff61705d67ab93b614bf8bdbe178a40aa73e1692
Malin Classon <m.classon@hotmail.com>
2017-02-27 04:29:20 +0100

    ..

--- ovning15.js
+++ ovning15.js
@@ -19,14 +19,20 @@ class App extends React.Component {
 
     this.setState({
       input1: value1,
-      input2: value2     
+      input2: value2,
+      errormessage: '',     
     }, () => this.getSum())
   }
 
   choiceOfOperator(event){
+  let value1 = document.getElementById('input1').value;
+  let value2 = document.getElementById('input2').value; 
 
     let op = event.target.value;
     this.setState({
+      input1: value1,
+      input2: value2,
+      errormessage: '',  
       operator: op
     }, () => this.getSum())
 
@@ -34,48 +40,91 @@ class App extends React.Component {
   }
 
 getSum(){
- let input1 = parseFloat(this.state.input1);
- let input2 = parseFloat(this.state.input2);
+ let input1 = this.state.input1;
+ let input2 = this.state.input2;
  let sum = 0;
- 
-if(Number.isNaN(input1) || Number.isNaN(input2) ){
-  this.setState({errormessage: 'Ogiltiga tal', result: NaN })
-}
 
-else if(this.state.operator === 'add'){
-  sum = input1 + input2;
-}
-else if(this.state.operator === 'subtract'){
-  sum = input1 - input2
-}
-else if(this.state.operator === 'divide'){
-  sum = input1 / input2
-}
-else if(this.state.operator === 'multiply'){
-  sum = input1 * input2
-}
-  this.setState({result: sum, errormessage: ''});
-}
+ 
+for (var i=0; i < input1.length; i++) { 
+     if(input1.charAt(i) !== '0'){
+       break;
+     }
+   else{
+     input1 = input1.substring(1);
+   }}
+  for (var i=0; i < input1.length; i++) { 
+     if(input2.charAt(i) !== '0'){
+       break;
+     }
+   else{
+     input2 = input1.substring(1);
+   }}
+
+
+ if(!isFinite(input1) || !isFinite(input2))
+ { 
+   this.setState({ errormessage:  'Ange två tal som är giltiga' , result: 'Error'}); 
+   }
+
+   else{
+      input1 = parseFloat(this.state.input1);
+      input2 = parseFloat(this.state.input2);
+
+
+  if(this.state.operator === 'divide' && input2 == 0) {
+    this.setState({errormessage: 'Not allowed to divide by zero', result:'Error'})
+  }
+   
+  else{
+    if(this.state.operator === 'add')
+    { 
+      sum = input1 + input2; 
+    } 
+    else if(this.state.operator === 'subtract')
+    { 
+      sum = input1 - input2
+    } 
+    else if(this.state.operator === 'divide')
+    { 
+        sum = input1 / input2 
+    } 
+    else if(this.state.operator === 'multiply')
+    { 
+      sum = input1 * input2 
+    } 
+    this.setState({result: sum, errormessage:'' }); }
+
+
+}   }
 
   
   render() {
     return (
-      <div id='app'>
-			  <FormComponent>
-		      <input id='input1' type='text' value={this.state.input1} onChange={this.inputChange}/> 
+      <div className="row">
+        <div className="col-md-offset-2">
+          <h1>Övning 15</h1>
+          <div className="col-md-8 form-container container">
+            <h3>Calculator</h3>
+            <FormComponent>
+              <div className="form-group">
+                <input id='input1' className="form-control" type='text' value={this.state.input1} onChange={this.inputChange}/>
 
-          <select value={this.state.operator} onChange={this.choiceOfOperator}>
-            <option value="add">+</option>
-            <option value="subtract">-</option>
-            <option value="divide">/</option>
-            <option value="multiply">*</option>
-          </select>
+                <select className="form-control" value={this.state.operator} onChange={this.choiceOfOperator}>
+                                <option value="add">+</option>
+                                <option value="subtract">-</option>
+                                <option value="divide">/</option>
+                                <option value="multiply">*</option>
+                              </select>
 
-				  <input id='input2' type='text' value={this.state.input2} onChange={this.inputChange} /> = 
-          <ResultComponent result = {this.state.result} />          
-		    </FormComponent>
-        <p>{this.state.errormessage}</p>
-			</div>
+                <input id='input2' className='form-control' type='text' value={this.state.input2} onChange={this.inputChange} />          <span className="eq">=</span>
+                <ResultComponent result={ this.state.result} />
+              </div>
+            </FormComponent>
+                    
+              <h3>{this.state.errormessage}</h3>
+          </div>
+        </div>
+      </div>
     )
   }
 }
@@ -83,14 +132,14 @@ else if(this.state.operator === 'multiply'){
 class FormComponent extends React.Component {
   render() {
     return (
-      <form>{this.props.children}</form>)
+      <form className='form-inline'>{this.props.children}</form>)
   };
 }
 
 class ResultComponent extends React.Component{
   render(){
     return(
-      <input type='text' readOnly value={this.props.result}/>  
+      <input type='text' className='form-control' readOnly value={this.props.result}/>  
   )}
 }
 
@@ -100,4 +149,3 @@ ReactDOM.render (
 	  <App/>,
   document.getElementById('app')
 )
-
